df1cfddbdc21b5e06d5588d28d959674c2bcbc95
wuyenlin <finne0831@gmail.com>
2021-02-20 10:45:20 +0100

    added myPE (repeat d_model)

--- common/transformer.py
+++ common/transformer.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+
+
 import os, math, copy
 import itertools
 import matplotlib.pyplot as plt
@@ -30,23 +33,49 @@ class PositionalEncoder(nn.Module):
         self.pe = pe
  
     def forward(self, x):
-        # make embeddings relatively larger
-        # x = x * math.sqrt(self.d_model)
+        # x *= math.sqrt(self.d_model) # scale it back up
         bs, seq_len = x.size(0), x.size(1)
         pe = self.pe[:, :seq_len, :]
 
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        pe_all = Variable(torch.zeros(bs, seq_len, self.d_model), requires_grad=True).to(device)
-        for i in range(bs):
-            pe_all[i, :, :] = pe
+        pe_all = pe.repeat(bs,1,1)
+        pe_all = pe_all.to(device)
 
         assert x.shape == pe_all.shape, "{},{}".format(x.shape, pe_all.shape)
         x += pe_all
         return x
 
+class myPE(nn.Module):
+    def __init__(self, d_model, bs=128, max_seq_len=1000, dropout=0.1):
+        super().__init__()
+        self.d_model = d_model
+        self.dropout = nn.Dropout(dropout)
+        # create constant 'pe' matrix with values dependant on 
+        # pos and i
+        pe = torch.zeros(max_seq_len, bs)
+        for pos in range(max_seq_len):
+            for i in range(0, bs, 2):
+                pe[pos, i] = math.sin(pos*math.pi / (10000 ** ((2 * i)/bs)))
+                pe[pos, i + 1] = math.cos(pos*math.pi / (10000 ** ((2 * (i + 1))/bs)))
+        pe = pe.unsqueeze(0).permute(2,1,0)
+        pe = pe.repeat(1,1,d_model)
+        self.pe = pe
+ 
+    def forward(self, x):
+        # x *= math.sqrt(self.d_model) # scale it back up
+        bs, seq_len = x.size(0), x.size(1)
+        pe = self.pe[:bs, :seq_len, :]
+
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        pe = pe.to(device)
+
+        assert x.shape == pe.shape, "{},{}".format(x.shape, pe.shape)
+        x = x + pe
+        return x
+
 class TransformerEncoderLayer(nn.Module):
     def __init__(self, d_model, nhead, dim_feedforward=128, dropout=0.1,
-                activation="relu", normalize_before=False):
+                activation="relu"):
         
         super().__init__()
         self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
@@ -61,16 +90,14 @@ class TransformerEncoderLayer(nn.Module):
         self.dropout2 = nn.Dropout(dropout)
 
         self.activation = _get_activation_fn(activation)
-        self.normalize_before = normalize_before
         
         self.d_model = d_model
-        self.pe = PositionalEncoder(d_model)
+        # self.pe = PositionalEncoder(d_model)
+        self.pe = myPE(d_model)
 
-    def forward_post(self,
-                     src,
-                     src_mask= None,
-                     src_key_padding_mask= None,
-                     pos= None):
+    def forward(self, src, src_mask= None,
+                src_key_padding_mask= None, pos= None):
+
         q = k = self.pe(src)
         src2 = self.self_attn(q, k, value=src, attn_mask=src_mask,
                               key_padding_mask=src_key_padding_mask)[0]
@@ -81,30 +108,8 @@ class TransformerEncoderLayer(nn.Module):
         src = self.norm2(src)
         return src
 
-    def forward_pre(self, src,
-                    src_mask= None,
-                    src_key_padding_mask= None,
-                    pos= None):
-        src2 = self.norm1(src)
-        q = k = self.with_pos_embed(src2, pos)
-        src2 = self.self_attn(q, k, value=src2, attn_mask=src_mask,
-                              key_padding_mask=src_key_padding_mask)[0]
-        src = src + self.dropout1(src2)
-        src2 = self.norm2(src)
-        src2 = self.linear2(self.dropout(self.activation(self.linear1(src2))))
-        src = src + self.dropout2(src2)
-        return src
-
-    def forward(self, src,
-                src_mask= None,
-                src_key_padding_mask= None,
-                pos= None):
-        if self.normalize_before:
-            return self.forward_pre(src, src_mask, src_key_padding_mask, pos)
-        return self.forward_post(src, src_mask, src_key_padding_mask, pos)
 
 class TransformerEncoder(nn.Module):
-
     def __init__(self, encoder_layer, num_layers, norm=None):
         super().__init__()
         self.layers = _get_clones(encoder_layer, num_layers)
@@ -119,7 +124,8 @@ class TransformerEncoder(nn.Module):
 
         for layer in self.layers:
             output = layer(output, src_mask=mask,
-                           src_key_padding_mask=src_key_padding_mask, pos=pos)
+                           src_key_padding_mask=src_key_padding_mask,
+                           pos=pos)
 
         if self.norm is not None:
             output = self.norm(output)
@@ -130,36 +136,28 @@ class Transformer(nn.Module):
     def __init__(self, d_model=30, nhead=3, num_encoder_layers=6,
                  dim_feedforward=128, dropout=0.1,
                  num_joints_in=15, num_joints_out=15,
-                 activation="relu", normalize_before=False,
-                 return_intermediate_dec=False):
+                 activation="relu"):
         super().__init__()
 
         encoder_layer = TransformerEncoderLayer(d_model, nhead, dim_feedforward,
-                                                dropout, activation, normalize_before)
-        encoder_norm = nn.LayerNorm(d_model) if normalize_before else None
-        self.encoder = TransformerEncoder(encoder_layer, num_encoder_layers, encoder_norm)
+                                                dropout, activation)
+        self.encoder = TransformerEncoder(encoder_layer, num_encoder_layers)
         self.linear = nn.Linear(num_joints_in*2, num_joints_out*3, bias=False)
 
         self.d_model = d_model
         self.nhead = nhead
         self.num_joints_out = num_joints_out
-        self.pe = PositionalEncoder(d_model)
 
     def forward(self, src, mask=None, query_embed=None, pos_embed=None):
-        # flatten NxCxHxW to HWxNxC
         bs = src.shape[0]
         src = torch.flatten(src, start_dim=2)
-        # pos_embed = pos_embed.flatten(2).permute(2, 0, 1)
 
         memory = self.encoder(src, src_key_padding_mask=mask, pos=pos_embed)
         output = self.linear(memory)
         ran = [13, output.size(1)-13]
         output = output[:, ran[0]:ran[1], :] # [b,n-26,45]
-        # print(output.shape)
 
-        # return hs.transpose(1, 2), memory.permute(1, 2, 0).view(bs, c, h, w)
         return output.reshape(bs, -1, self.num_joints_out, 3)
-
 
 
 def _get_clones(module, N):
