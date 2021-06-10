60c223b753909bea2f2ff53dc0d721b987885009
wuyenlin <finne0831@gmail.com>
2021-06-10 16:49:19 +0800

    updated run file

--- this.sh
+++ this.sh
@@ -1,1 +1,1 @@
-python3 super.py -d humaneva15 -k detectron_pt_coco -str Train/S1,Train/S2,Train/S3 -ste Validate/S1,Validate/S2,Validate/S3 -b 128 -e 1000 -lrd 0.996 -lr 0.01 -a Walk,Jog,Box --by-subject
+python3 super.py -d humaneva15 -k detectron_pt_coco -str Train/S1,Train/S2,Train/S3 -ste Validate/S1,Validate/S2,Validate/S3 -b 128 -e 1000 -lrd 0.996 -lr 0.0005 -a Walk,Jog,Box --by-subject --export-training-curves 
