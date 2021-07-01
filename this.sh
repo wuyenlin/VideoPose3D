de0e81ccdba6f1900b4d4ac9ffd57cc460dff3e1
wuyenlin <finne0831@gmail.com>
2021-07-01 09:39:12 +0800

    run with rf=1

--- this.sh
+++ this.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#python3 super.py -d humaneva15 -k detectron_pt_coco -str Train/S1,Train/S2,Train/S3 -ste Validate/S1,Validate/S2,Validate/S3 -b 128 -e 1000 -lrd 0.996 -lr 0.0005 -a Walk,Jog,Box --by-subject --export-training-curves 
-#CMD="python3 run.py -d humaneva15 -k detectron_pt_coco -str Train/S1,Train/S2,Train/S3 -ste Validate/S1,Validate/S2,Validate/S3 -a Walk,Jog,Box --by-subject -c checkpoint --evaluate pretrained_humaneva15_detectron.bin"
-CMD="python3 run.py -k cpn_ft_h36m_dbb -arc 3,3,3,3,3 -c checkpoint --evaluate pretrained_h36m_cpn.bin"
+# eval
+#CMD="python3 run.py -k cpn_ft_h36m_dbb -arc 3,3,3,3,3 -c checkpoint --evaluate pretrained_h36m_cpn.bin"
 
+# training from scratch
+CMD='python3 run.py -e 80 -k cpn_ft_h36m_dbb -arc 1'
 echo $CMD
 $CMD
